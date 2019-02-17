805419a331a0cd7ca0f710a42a9cd8cd4a2bd0a3
D2ntes <gruzdev-n@mail.ru>
2019-02-17 23:11:05 +0300

    -добавлены исключения

--- py_21_diplom.py
+++ py_21_diplom.py
@@ -22,7 +22,7 @@ class ScriptError(Exception):
 class User:
     def __init__(self):
         self.short_name = ''
-        self.id = -1
+        self.id = 0
         self.friends = []
         self.communities = set()
         self.friends_communities = set()
@@ -31,55 +31,65 @@ class User:
         self.token = 'ed1271af9e8883f7a7c2cefbfddfcbc61563029666c487b2f71a5227cce0d1b533c4af4c5b888633c06ae'
 
     def input_user(self):
-        name = input('Введите имя пользователя или его id в ВК: ')
-        if not str(name).isdigit():
-            response = self.call_api('utils.resolveScreenName', {'screen_name': name})
-            if response['type'] == 'user':
-                self.id = response['object_id']
-        else:
-            self.id = name
+        while not self.id:
+            try:
+                name = input('Введите имя пользователя или его id в ВК: ')
+                if not str(name).isdigit():
+                    response = self.call_api('utils.resolveScreenName', {'screen_name': name})
+                    if response['type'] == 'user':
+                        self.id = response['object_id']
+                else:
+                    self.id = name
+            except Exception:
+                print('Введены некорректные данные')
 
     def get_friends(self):
-        self.friends = self.call_api('friends.get', {'user_id': self.id})['items']
+        try:
+            self.friends = self.call_api('friends.get', {'user_id': self.id})['items']
+        except Exception as e:
+            pass
 
     def get_communities(self, id_users):
         if not isinstance(id_users, list):
             id_users = [id_users]
         communities = []
         errors = 0
-        tqdm.write('Получаем список групп', file=sys.stdout)
+        tqdm.write(f'\nПолучаем список групп', file=sys.stdout)
         for user in tqdm(iterable=id_users, unit='pcs', file=sys.stdout):
             try:
                 response = self.call_api('groups.get', {'user_id': user})
                 communities.extend(response['items'])
             except Exception:
                 errors += 1
-                # with open(f'errors_log.txt', 'a') as f:
-                #     tqdm.write(f"{time.asctime()} {req}\n", file=f)
+
         if errors:
             tqdm.write(f'Количество ошибок при запросах: {errors}', file=sys.stdout)
-        tqdm.write(f'Выполнено успешно. Кол-во групп: {len(set(communities))}\n', file=sys.stdout)
+        tqdm.write(f'Выполнено успешно. Кол-во групп: {len(set(communities))}', file=sys.stdout)
         return set(communities)
 
     def find_similar_communities(self):
         print('Получаем общие id групп')
         self.unique_groups = self.communities - self.friends_communities
-        print(f'Кол-во общих групп: {len(self.unique_groups)}\n')
 
     def communties_info(self, communities):
+
         communities_str = ','.join(map(str, communities))
         communities_info = self.call_api('groups.getById', {'fields': 'members_count', 'group_ids': communities_str})
         target_communities_info = list()
         new_community = dict()
-
         print('Обрабатываем необходимые данные о группах')
+
         for group in tqdm(communities_info, unit='pcs', file=sys.stdout):
-            new_community['name'] = group['name']
-            new_community['gid'] = group['id']
-            new_community['members_count'] = group['members_count']
-            target_communities_info.append({'name': group['name'], 'gid': group['id'],
-                                            'members_count': group['members_count']})
-            time.sleep(0.5)
+            try:
+                new_community['name'] = group['name']
+                new_community['gid'] = group['id']
+                new_community['members_count'] = group['members_count']
+                target_communities_info.append({'name': group['name'], 'gid': group['id'],
+                                                'members_count': group['members_count']})
+                time.sleep(0.5)
+
+            except Exception as e:
+                pass
         print()
         if self.unique_groups:
             print(f'Количество групп в ВК в которых состоит пользователь(https://vk.com/id{self.id}), '
@@ -112,6 +122,8 @@ class User:
             response = response.json()['response']
         except Exception:
             pass
+            # with open(f'errors_log.txt', 'a') as f:
+            #     tqdm.write(f"{time.asctime()} {req}\n", file=f)
         return response
 
 
