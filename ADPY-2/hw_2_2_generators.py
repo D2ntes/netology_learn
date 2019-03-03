6e882b3fe72bb2826e7cfc785d0cf27ab4fb6c5b
D2ntes <gruzdev-n@mail.ru>
2019-03-04 00:33:26 +0300

    ADD: hw_2_2_generators.py # Домашнее задание к лекции 1.2 «Iterators. Generators. Yield» # Написать генератор, который принимает путь к файлу. При каждой итерации возвращает md5 хеш каждой строки файла.

--- ADPY-2/hw_2_2_generators.py
+++ ADPY-2/hw_2_2_generators.py
@@ -6,14 +6,17 @@
 import hashlib
 
 
-def hash_lines(filename=r'url_wiki_countries.txt'):
-    with open(filename, 'rb') as datafile:
-        m = hashlib.md5()
-        line = datafile.readline()
-        while line:
-            m.update(line)
+def hash_lines(filename):
+    try:
+        with open(filename, 'rb') as datafile:
+            m = hashlib.md5()
             line = datafile.readline()
-            yield m.hexdigest()
+            while line:
+                m.update(line)
+                line = datafile.readline()
+                yield m.hexdigest()
+    except FileNotFoundError as text_error:
+        print(text_error)
 
 if __name__ == '__main__':
     for number, hash_line in enumerate(hash_lines(input('Введите путь к файлу: ')), 1):
