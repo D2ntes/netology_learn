7c6b264049e28dc141848a72d2fde7fcfbfb693e
D2ntes <gruzdev-n@mail.ru>
2019-05-27 20:32:11 +0300

    Diplom ADPY-2 v0.5

---
+++ ADPY-2/diplom/db/db_config.py
@@ -0,0 +1,16 @@
+# Параметры подключения к базе данных
+
+HOST = 'localhost'
+PORT = 27017
+
+# Параметры базы данных
+
+RES_DB = 'vkinder_db'
+RES_COL = 'result'
+OFF_COL = 'offset'
+
+# Смещение по умолчанию
+
+START = 0
+STEP = 100
+STOP = 1000
---
+++ ADPY-2/diplom/score/defaults.py
@@ -0,0 +1,13 @@
+# Паттерн для анализа интересов, музыки, фильмов, телепередач, книг
+
+DEFAULT_PATTERN = r'[!@#$%^&*()№±_=\\\|\'\"\-\+\}\{\[\]\<\>\?\/]'
+
+# Вес критериев поиска
+
+INTERESTS_SCORE = 35
+FRIENDS_SCORE = 30
+GROUPS_SCORE = 25
+MOVIES_SCORE = 20
+MUSIC_SCORE = 15
+BOOKS_SCORE = 5
+TV_SCORE = 10
