f2f6d33bf6517979294a4365f0a40f3e039e52c5
D2ntes <gruzdev-n@mail.ru>
2019-12-19 01:39:40 +0300

    Completed dj-homeworks/request-handling/file_server/

--- DJ-7/request-handling/file_server/app/views.py
+++ DJ-7/request-handling/file_server/app/views.py
@@ -1,29 +1,45 @@
-import datetime
-
 from django.shortcuts import render
+from django.views.generic import TemplateView
+
+from django.conf import settings
+from django.http import HttpResponseNotFound
+
+import os
+from datetime import datetime as dt
 
 
-def file_list(request):
+class FileList(TemplateView):
     template_name = 'index.html'
-    
-    # Реализуйте алгоритм подготавливающий контекстные данные для шаблона по примеру:
-    context = {
-        'files': [
-            {'name': 'file_name_1.txt',
-             'ctime': datetime.datetime(2018, 1, 1),
-             'mtime': datetime.datetime(2018, 1, 2)}
-        ],
-        'date': datetime.date(2018, 1, 1)  # Этот параметр необязательный
-    }
 
-    return render(request, template_name, context)
+    def get_context_data(self, date=None):
+        # Реализуйте алгоритм подготавливающий контекстные данные для шаблона по примеру:
+        files_list = os.listdir(settings.FILES_PATH)
+        result = {'files': [], 'date': date}
+        for item in files_list:
+            file_info = os.stat(os.path.join(settings.FILES_PATH, item))
+            file_data = {
+                'name': item,
+                'ctime': dt.utcfromtimestamp(file_info.st_ctime),
+                'mtime': dt.utcfromtimestamp(file_info.st_mtime)
+            }
+            if date:
+                if file_data['ctime'].date() == dt.strptime(date, '%Y-%m-%d').date():
+                    result['files'].append(file_data)
+            else:
+                result['files'].append(file_data)
+        return result
 
 
 def file_content(request, name):
     # Реализуйте алгоритм подготавливающий контекстные данные для шаблона по примеру:
-    return render(
-        request,
-        'file_content.html',
-        context={'file_name': 'file_name_1.txt', 'file_content': 'File content!'}
-    )
-
+    file_path = os.path.join(settings.FILES_PATH, name)
+    if os.path.exists(file_path):
+        with open(file_path, encoding='utf8') as contents:
+            file_contents = contents.read()
+            return render(
+                request,
+                'file_content.html',
+                context={'file_name': name, 'file_content': file_contents}
+            )
+    else:
+        return HttpResponseNotFound('404. Not found')
