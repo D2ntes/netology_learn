c9029b6773ef10febce766843e327caebd1c6187
D2ntes <gruzdev-n@mail.ru>
2019-12-19 00:14:14 +0300

    Completed dj-homeworks/request-handling/landing/

--- DJ-7/request-handling/landing/app/views.py
+++ DJ-7/request-handling/landing/app/views.py
@@ -1,6 +1,6 @@
 from collections import Counter
 
-from django.shortcuts import render_to_response
+from django.shortcuts import render
 
 # Для отладки механизма ab-тестирования используйте эти счетчики
 # в качестве хранилища количества показов и количества переходов.
@@ -12,15 +12,24 @@ counter_click = Counter()
 
 def index(request):
     # Реализуйте логику подсчета количества переходов с лендига по GET параметру from-landing
-    return render_to_response('index.html')
+    from_landing = request.GET.get('from-landing')
+    if from_landing == 'original':
+        counter_click['original'] += 1
+    elif from_landing == 'test':
+        counter_click['test'] += 1
+    return render(request, 'index.html')
+
 
 
 def landing(request):
-    # Реализуйте дополнительное отображение по шаблону app/landing_alternate.html
-    # в зависимости от GET параметра ab-test-arg
-    # который может принимать значения original и test
-    # Так же реализуйте логику подсчета количества показов
-    return render_to_response('landing.html')
+    ab_test_arg = request.GET.get('ab-test-arg')
+    if ab_test_arg == 'original':
+        counter_show['original'] += 1
+        return render(request, 'landing.html')
+    elif ab_test_arg == 'test':
+        counter_show['test'] += 1
+        return render(request, 'landing_alternate.html')
+
 
 
 def stats(request):
@@ -28,7 +37,13 @@ def stats(request):
     # Чтобы отличить с какой версии лендинга был переход
     # проверяйте GET параметр marker который может принимать значения test и original
     # Для вывода результат передайте в следующем формате:
-    return render_to_response('stats.html', context={
-        'test_conversion': 0.5,
-        'original_conversion': 0.4,
+    original_conversion = 0 if counter_show['original'] is 0 \
+        else counter_click['original'] / counter_show['original']
+
+    test_conversion = 0 if counter_show['test'] is 0 \
+        else counter_click['test'] / counter_show['test']
+
+    return render(request, 'stats.html', context={
+        'test_conversion': test_conversion,
+        'original_conversion': original_conversion
     })
