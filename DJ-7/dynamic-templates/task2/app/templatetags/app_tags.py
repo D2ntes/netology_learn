9e8dc1275dd674038dca09ade3e5f753a22e64a5
D2ntes <gruzdev-n@mail.ru>
2019-12-26 02:19:01 +0300

    Completed dj-homeworks/dynamic-templates/task2/

--- DJ-7/dynamic-templates/task2/app/templatetags/app_tags.py
+++ DJ-7/dynamic-templates/task2/app/templatetags/app_tags.py
@@ -3,3 +3,10 @@ from django import template
 
 register = template.Library()
 
+
+@register.simple_tag
+def highlight(request, path):
+    if path in request.path:
+        return 'active'
+    else:
+        return 'pass'
