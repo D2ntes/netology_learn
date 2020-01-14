b6224eee169dc672aecd4b48f67c494fff054170
D2ntes <46005116+D2ntes@users.noreply.github.com>
2020-01-14 12:11:34 +0300

    Completed dj-homeworks/first-project/databases_2/

--- DJ-7/databases_2/orm_migrations/school/views.py
+++ DJ-7/databases_2/orm_migrations/school/views.py
@@ -1,13 +1,12 @@
 from django.views.generic import ListView
 from django.shortcuts import render
 
-from .models import Student
+from .models import Student, Teacher
 
 
 def students_list(request):
     template = 'school/students_list.html'
     ordering = 'group'
-    student_list = Student.objects.all().prefetch_related('teacher__students').order_by(ordering)
+    student_list = Student.objects.all().prefetch_related('teacher').order_by(ordering)
     context = {'student_list': student_list}
-    # print(context)
     return render(request, template, context)
